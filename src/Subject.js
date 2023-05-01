16e5dc747fe3c00860f0bc383b4e985fe4bda490
NakJi <nhl0113@kaist.ac.kr>
2023-05-02 00:30:53 +0900

    Bone

---
+++ src/Subject.js
@@ -0,0 +1,20 @@
+class Subject {
+  constructor(){this.observers=[]; }
+
+  subscribe (observer){
+      if (observer) this.observers.push(observer);
+  }
+  unsubscribeAll(){
+      this.observers=[];
+  }
+  unsubscribe(observer){
+      this.observers=this.observers.filter(o => o != observer);
+  }
+  notifySubscribers(source, ...others){
+      for (const observer of this.observers){
+          observer.update(source, ...others);
+      } 
+  }
+}
+
+export { Subject };
